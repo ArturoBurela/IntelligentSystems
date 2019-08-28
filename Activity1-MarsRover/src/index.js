9d69dc765024947adb38d6922a81157849eff254
Arturo Burela <uno@arturoburela.com>
2019-08-28 08:16:54 -0500

    First scene in docs

--- Activity1-MarsRover/src/index.js
+++ Activity1-MarsRover/src/index.js
@@ -1,9 +1,28 @@
-function component() {
-  const element = document.createElement('div');
+import * as THREE from 'three';
+const scene = new THREE.Scene();
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  return element;
-}
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(component());
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00
+});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+var animate = function() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
