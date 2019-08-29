c795dbb17646088693bd79b9413a042425e0f8c5
Arturo Burela <uno@arturoburela.com>
2019-08-28 20:00:06 -0500

    Add assets and object loader

--- Activity1-MarsRover/src/index.js
+++ Activity1-MarsRover/src/index.js
@@ -1,22 +1,43 @@
-import * as THREE from 'three';
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-var renderer = new THREE.WebGLRenderer();
+// Import libraries
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh
+} from 'three';
+import {
+  GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader';
+// Create scene, camera and renderer
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new WebGLRenderer();
+// Bind renderer to html
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({
+// Instantiate a loader
+var loader = new GLTFLoader();
+
+loader.load('assets/rover.glb', function(gltf) {
+  scene.add(gltf.scene);
+}, undefined, function(error) {
+  console.error(error);
+});
+
+// Cube example for now, replace with env and agent
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({
   color: 0x00ff00
 });
-var cube = new THREE.Mesh(geometry, material);
+const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-var animate = function() {
+const animate = function() {
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.01;
