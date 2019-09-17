8ab4c7c9eadd4eaf1d0499078f40184c8b00ddcb
El FAStidio <fernandoalcasan@outlook.com>
2019-09-16 22:38:26 -0500

    fixed random movement when reaching known rocks

---
+++ docs/MarsRover/MultiAgent/agent.js
@@ -0,0 +1,185 @@
+import {
+  GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader';
+
+import { Box3 } from "three";
+
+class RoverAgent {
+  constructor(scene, x, y, z) {
+    this.scene = scene;
+    // Instantiate a loader GLTF is prefered
+    this.loader = new GLTFLoader();
+    this.modelAgent;
+    this.addAgent(x, y, z, scene);
+    this.rockStack = 0;
+    this.rockLimit = 6;
+    this.rocksCollected = 0;
+    this.full = false;
+    this.velocity = 3;
+    this.rotate = true;
+    this.stop = false;
+  }
+
+  addAgent(x, y, z, scene) {
+    const ctx = this;
+    this.loader.load('assets/rover.glb', function (gltf) {
+      gltf.scene.position.z = z;
+      gltf.scene.position.y = y;
+      gltf.scene.position.x = x;
+      gltf.scene.scale.set(50,50,50);
+      gltf.scene.rotation.set(0,0,0);
+      //console.log(gltf.scene);
+      let newBox = new Box3().setFromObject(gltf.scene);
+      ctx.modelAgent = gltf.scene;
+      ctx.modelAgent.collider = newBox;
+      ctx.scene.add(ctx.modelAgent);
+      //console.log('agente creado');
+      scene.agentsLoaded += 1;
+      //console.log('numero de agentes cargados: ' + scene.agentsLoaded.toString());
+    }, undefined, function (error) {
+      console.error(error);
+    });
+  }
+
+  moveAgent() {
+    const ctx = this;
+    if(ctx.stop == true){
+      return;
+    }
+    //console.log('rotation of agent is: ' + ctx.modelAgent.rotation.y.toString());
+    if (ctx.modelAgent.rotation.y === 0) {
+      ctx.modelAgent.position.z += ctx.velocity;
+    } else if (ctx.modelAgent.rotation.y === (Math.PI / 2)) {
+      ctx.modelAgent.position.x += ctx.velocity;
+    } else if (ctx.modelAgent.rotation.y === Math.PI) {
+      ctx.modelAgent.position.z -= ctx.velocity;
+    } else if (ctx.modelAgent.rotation.y === ((3*Math.PI) / 2)){
+      ctx.modelAgent.position.x -= ctx.velocity;
+    }
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  rotateAgent() {
+    let randomNum = Math.random();
+    const ctx = this;
+    if (randomNum >= 0 && randomNum < 0.25) {
+      ctx.modelAgent.rotation.y = 0;
+    } else if (randomNum >= 0.25 && randomNum < 0.5) {
+      ctx.modelAgent.rotation.y = Math.PI / 2;
+    } else if (randomNum >= 0.5 && randomNum < 0.75) {
+      ctx.modelAgent.rotation.y = Math.PI;
+    } else {
+      ctx.modelAgent.rotation.y = (3*Math.PI) / 2;
+    }
+  }
+
+  //Cuando se colisiona con la nave/estación
+  updateBase(collider) {
+    if(this.modelAgent.collider.intersectsBox(collider)){
+      //console.log("lol");
+      if(this.rockStack > 0) {
+        //console.log('A total of ' + this.rockStack.toString() + ' more rocks have been left at the station.');
+        this.rocksCollected += this.rockStack;
+        //console.log('The total of rocks left by this agent at the station is: ' + this.rocksCollected.toString() + ' rocks.');
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //Cuando se colisiona con una roca
+  updateRock(col){
+    if(this.modelAgent.collider.intersectsBox(col)){
+      if(this.rockStack == this.rockLimit){
+        return false;
+      }
+      else {
+        this.rockStack += 1;
+        this.scene.rocksGathered += 1;
+        //console.log('Rock stack has ' + this.rockStack.toString() + ' rocks');
+        if(this.rockStack == this.rockLimit) {
+          //console.log('Rock stack limit has been already reached, agent must go to the station!');
+          this.full = true;
+        }
+        return true;
+      }
+      //console.log("Rock Found!!!");
+    }
+    return false;
+  }
+
+  //Cuando se colisiona con un obstáculo
+  updateObstacle(col){
+    if(this.modelAgent.collider.intersectsBox(col)){
+      //console.log("Obstacle Found!!!");
+      var selection = Math.round(Math.random());
+      this.avoidObstacle(selection);
+    }
+  }
+
+  updateOtherAgent(col){
+    if(this.modelAgent.collider.intersectsBox(col)){
+      //console.log("Collision with another agent detected, changing direction.");
+      var selection = Math.round(Math.random());
+      this.avoidObstacle(selection);
+    }
+  }
+
+  avoidObstacle(selection){
+    const ctx = this;
+    if (ctx.modelAgent.rotation.y === 0 || ctx.modelAgent.rotation.y === Math.PI) {
+
+      if(ctx.modelAgent.rotation.y === 0){
+        ctx.modelAgent.position.z -= 5;
+      }
+      else if(ctx.modelAgent.rotation.y === Math.PI){
+        ctx.modelAgent.position.z += 5;
+      }
+
+      if (selection === 0){
+        ctx.modelAgent.rotation.y = Math.PI / 2;
+      }
+      else{
+        ctx.modelAgent.rotation.y = (3*Math.PI) / 2;
+      }
+
+    } else if (ctx.modelAgent.rotation.y === (Math.PI / 2) || ctx.modelAgent.rotation.y === ((3*Math.PI) / 2)) {
+
+      if(ctx.modelAgent.rotation.y === (Math.PI / 2)){
+        ctx.modelAgent.position.x -= 5;
+      }
+      else if(ctx.modelAgent.rotation.y === ((3*Math.PI) / 2)){
+        ctx.modelAgent.position.x += 5;
+      }
+
+      if (selection === 0){
+        ctx.modelAgent.rotation.y = 0;
+      }
+      else{
+        ctx.modelAgent.rotation.y = Math.PI;
+      }
+    }
+  }
+
+  updateLimits(){
+    const ctx = this;
+    if(ctx.modelAgent.position.z >= 1000 || ctx.modelAgent.position.z <= -1600 ||
+       ctx.modelAgent.position.x >= 1150 || ctx.modelAgent.position.x <= -1150){
+         //console.log('Limit of map has been reached, go other way');
+         ctx.avoidObstacle(Math.round(Math.random()));
+    }
+  }
+
+  animate() {
+    if(this.modelAgent && this.modelAgent.collider) {
+      this.modelAgent.collider.setFromObject(this.modelAgent);
+    }
+  }
+}
+
+export {
+  RoverAgent
+};
--- docs/MarsRover/MultiAgent/index.js
+++ docs/MarsRover/MultiAgent/index.js
@@ -22,16 +22,15 @@ import {
 //const canvas = document.createElement( 'canvas' );
 const canvas = document.getElementById("canvas");
 // Try to use WEBGL2, fallback to default context
-const context = WEBGL.isWebGL2Available() ? canvas.getContext('webgl2', { alpha: false }) : canvas.getContext();
+const context = WEBGL.isWebGL2Available() ? canvas.getContext( 'webgl2', { alpha: false } ) : canvas.getContext();
 // Create basic ThreeJS objects: scene, camera, controls and renderer
 var scene = new Scene();
 scene.agentsLoaded = 0;
 scene.rocksGathered = 0;
 var clock = new Clock();
-var multiagente = true;
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 const renderer = new WebGLRenderer({ canvas: canvas, context: context });
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls( camera, renderer.domElement );
 // Bind renderer to html
 renderer.setSize(window.innerWidth, window.innerHeight);
 //document.body.appendChild(renderer.domElement);
@@ -39,12 +38,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // Set camera position
 // camera.position.z = 500;
 // camera.position.y = 500;
-camera.position.set(9.6, 1242.3, -158.76);
-camera.rotation.y = (3 * Math.PI) / 2;
+camera.position.set(9.6,1242.3,-158.76);
 
 // renderer
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
 // container.appendChild( renderer.domElement );
 renderer.gammaInput = true;
 renderer.gammaOutput = true;
@@ -62,10 +60,9 @@ controls.maxDistance = 3500;
 var rocksSpawned = 20;
 var obstaclesSpawned = 5;
 // Instantiate Environment with number of rocks and number of obstacles
-var env = new MarsEnvironment(rocksSpawned, obstaclesSpawned, multiagente, scene);
+var env = new MarsEnvironment(rocksSpawned, obstaclesSpawned, scene);
 // Instantiate Agent?
 var agents = [];
-scene.agents = agents;
 //const agent = new RoverAgent(scene);
 var no_of_agents = 6;
 //let rotate = true;
@@ -74,24 +71,15 @@ var i;
 var finished = false;
 var end = false;
 // animate the scene
-const animate = function () {
+const animate = function() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   if(scene.agentsLoaded == no_of_agents) {
     // Call to env animate
     //env.animate();
-    //let mensajes = env.updateMessages();
-    //env.animate();
 
     for(i = 0; i < no_of_agents; i++){
       agents[i].animate();
-      /*if (agents[i].isCarrier && mensajes.length > 0) { // Mensajes en el ambiente para saber posiciones
-        if (agents[i].positions.length < 1){ // una roca por agente
-          agents[i].positions.push(mensajes[0]); // agregar mensaje a agente
-          mensajes.shift(); // eliminar de mensajes
-          agents[i].positions = [...new Set(agents[i].positions)]; // que sea único
-        }
-      }*/
     }
 
     if (env.marsBase) {
@@ -115,7 +103,6 @@ const animate = function () {
         finished = true;
       }
 
-
       for(i = 0; i < no_of_agents; i++){
 
         agents[i].moveAgent();
@@ -132,23 +119,8 @@ const animate = function () {
             //Remover el collider y el mesh de la roca en la variable env
             //console.log('The agent got a rock!');
             scene.remove(scene.getObjectByName(env.rocksColliders[x].name));
-
-            if(agents[i].positions.length > 0) {
-              if (agents[i].positions[0].x > env.rocksColliders[x].position.x - 10 && agents[i].positions[0].x < env.rocksColliders[x].position.x + 10
-                  && agents[i].positions[0].z > env.rocksColliders[x].position.z - 10 && agents[i].positions[0].z < env.rocksColliders[x].position.z + 10) {
-                var a;
-                for(a = 0; a < agents.length; a++){
-                  agents[a].deletePosition(env.rocksColliders[x].position);
-                  //agents[a].positions.shift();
-                }
-                //console.log('se borró una posición');
-              }
-            }
-
             env.rocksColliders.splice(x,1);
             env.rocks.splice(x,1);
-
-
           }
         }
         for(x = 0; x < env.ufos.length; x++)
@@ -160,12 +132,6 @@ const animate = function () {
         {
           if(x != i){
             agents[i].updateOtherAgent(agents[x].modelAgent.collider);
-          }
-        }
-
-        for (x = 0; x < agents.length; x++) {
-          if (agents[i].isCarrier && agents[i].positions.length > 0 && Math.floor(clock.getElapsedTime()) % 1.5 === 0) {
-            agents[i].goForRock(agents[i].positions[0]);
           }
         }
 
@@ -184,7 +150,7 @@ const animate = function () {
           }
         }
 
-        if(Math.floor(clock.getElapsedTime()) % 10 === 0 && clock.getElapsedTime() > 1 && agents[i].rotate && agents[i].full == false && agents[i].stop == false && agents[i].positions.length == 0){
+        if(Math.floor(clock.getElapsedTime()) % 10 === 0 && clock.getElapsedTime() > 1 && agents[i].rotate && agents[i].full == false && agents[i].stop == false){
           agents[i].rotate = false;
           agents[i].rotateAgent();
         } else if (Math.floor(clock.getElapsedTime()) % 10 === 0 && !agents[i].rotate) {
@@ -200,9 +166,9 @@ const animate = function () {
 
 };
 
-function loadAgent(a, isCarrier) {
+function loadAgent(a) {
   return new Promise(resolve => {
-    const newAgent = new RoverAgent(scene, (-642 + (233 * a)), 25, -250, isCarrier, env.multiagent, env);
+    const newAgent = new RoverAgent(scene, (-642 + (233 * a)), 25, -250);
     agents.push(newAgent);
     resolve();
   });
@@ -211,15 +177,7 @@ function loadAgent(a, isCarrier) {
 async function loadAll() {
   var i;
   for(i = 0; i < no_of_agents; i++){
-    if (multiagente) {
-      if (i < 3) {
-        await loadAgent(i, false);
-      } else {
-        await loadAgent(i, true);
-      }
-    } else {
-      await loadAgent(i,false);
-    }
+    await loadAgent(i);
   }
 }
 
