c5ec0eada063df7082734873191fa6dff864c36a
Arturo Burela <uno@arturoburela.com>
2019-09-26 19:12:56 -0500

    Assigment 3 not complete

--- Assigment 3/game.py
+++ Assigment 3/game.py
@@ -1,15 +1,20 @@
 import itertools
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class lampGame:
     def __init__(self):
         # List represents family members walk speed
-        self.initialSide = [1, 3, 6, 8, 12]
+        self.startSide = [1, 3, 6, 8, 12]
         # Side 2 is empty
         self.endSide = []
         # Lamp duration
-        self.lamp = 30
+        self.lampTime = 30
         # Lamp location
         self.lampSide = 1
+        self.df = pd.DataFrame({ 'from':[], 'to':[]})
 
     # Returns what persons to pick next
     def h1(self):
@@ -19,7 +24,7 @@ class lampGame:
         # take the shorter and larger number and cross
         if self.lampSide == 1:
             # For every posible pair to move
-            for pair in itertools.combinations(self.initialSide, r=2):
+            for pair in itertools.combinations(self.startSide, r=2):
                 # Find the cost -> cost of min + min between return
                 if len(self.endSide) != 0:
                     min1 = min(self.endSide)
@@ -42,41 +47,52 @@ class lampGame:
         return pairs, costs
 
     # Updates peoples location and lamp duration
-    def cross(self, p1, p2):
-        # Take person 1 and 2 from side 1 and move to side 2
+    def cross(self, side1, side2, people):
+        for p in people:
+            side1.remove(p)
+            side2.append(p)
+        return side1, side2
+
+    def addToDataFrame(self, node1, node2):
+        df2 = pd.DataFrame({ 'from':[node1], 'to':[node2]})
+        self.df = self.df.append(df2)
+
+    def bruteForce(self, startSide, endSide, lampTime, lampSide):
+        if lampTime == 0:
+            return
+        # Current node to draw the tree
+        currentNode = str(startSide) + str(endSide)
+        # Determine all the possible movements from the current point
         if self.lampSide == 1:
-            # Take person 1 and 2 from side 1 and move to side 2
-            self.initialSide.remove(p1)
-            self.initialSide.remove(p2)
-            self.endSide.append(p1)
-            self.endSide.append(p2)
-            self.lampSide = 0
-        # Take person 1 and 2 from side 2 and move to side 1
+            # For every posible pair to move
+            for pair in itertools.combinations(self.startSide, r=2):
+                # Determine state after move
+                side1, side2 = self.cross(startSide[:], endSide[:], pair)
+                nextNode = str(side1) + str(side2)
+                self.addToDataFrame(currentNode, nextNode)
+                # Decide where to go, brute force goes to all
+                # self.bruteForce(side1, side2,)
+        # If lamp is on side 2 return with the fastest person
         else:
-            self.endSide.remove(p1)
-            self.initialSide.append(p1)
-            self.lampSide = 1
-        # Longer walking speed is substracted to lamp
-        if p2 == None:
-            p2 = 0
-        self.lamp -= max(p1, p2)
+            pass
+            
+
+
+
+    def recursive(self, endSide, startSide):
+        pass
 
     def start(self):
-        # While not all people have crossed
-        while len(self.initialSide):
-            if self.lamp < 0:
-                print('SOLUTION NOT FOUND, EMPTY LAMP')
-                return -1
-            # Calculate heuristic
-            pairs, costs = self.h1()
-            # Move according to heuristic, get the min cost
-            mi = min(costs)
-            print(mi)
-            # If two mins go for the little
-            print('Chooosing ' + str(pairs[mi]))
-            self.cross(*pairs[mi])
-            print('Lamp left:' + str(self.lamp))
-        print('Done')
+        # Solve with depth first search
+        self.bruteForce(self.startSide[:], self.endSide[:], self.lampTime, self.lampSide)
+        # Print paths
+        print(self.df)
+        G=nx.from_pandas_edgelist(self.df, 'from', 'to')
+        # Fruchterman Reingold
+        nx.draw(G, with_labels=True, node_size=1500, node_color="skyblue", pos=nx.fruchterman_reingold_layout(G))
+        plt.title("fruchterman_reingold")
+        plt.show()
+
 
 game = lampGame()
 game.start()
